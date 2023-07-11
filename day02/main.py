451799995b656df31d67334b03c3b01826d68bb1
Sophia <sophiambataru@gmail.com>
2023-07-11 17:48:09 +0300

    Complete

--- day02/main.py
+++ day02/main.py
@@ -1,34 +1,27 @@
 # import the date class
-
 from datetime import date
 from datetime import datetime
 
-# calling the today
-# function of date class
-
+# call the today
 datetime.today()
 
 # date object of today's date
 today = datetime.today()
-
 type(today)
-
-
 
 today_date = date.today()
 
 today_date
-
-
 type(today_date)
 
+# obtain today's date (11-07-2023)
+today_date.day
 
 today_date.month
 
 today_date.year
 
-today_date.day
-
+# obtain Christmas' date
 christmas = date(today_date.year, 12, 25)
 christmas
 
